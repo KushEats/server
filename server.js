702e1ca337f08734819cdd7976ff6f1f76d814b6
Tim McCoy <timmymccoy@gmail.com>
2018-05-01 11:06:22 -0700

    began api route

--- server.js
+++ server.js
@@ -7,13 +7,10 @@ const pg = require('pg');
 const superagent = require('superagent');
 const bodyparser = require('body-parser');
 
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 const CLIENT_URL = process.env.CLIENT_URL;
 // const TOKEN = process.env.TOKEN;
-
-
 
 const API_KEY = process.env.GOOGLE_API_KEY;
 
@@ -26,14 +23,17 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/api/v1/admin', (req, res)
+
+//https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&key=AIzaSyDLlLO9AmKusj7eGw_rT8e2t7MYLROTG_I
+
+
 // app.get('/api/v1/admin', (req, res) => res.send(TOKEN === parseInt(req.query.token)));
 
 
 // app.get('/api/v1/map/stores', (req, res) => res.send);
 
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
-
-
 
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
