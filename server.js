bdf1002587e29ea79cc17a74e616ec83b669e515
Stephen <stephendeanharper@gmail.com>
2018-05-03 13:42:33 -0700

    deleted zombie code and added in comment for if statement for home-view

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const yelp = require('yelp-fusion');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const CLIENT_URL = process.env.CLIENT_URL;
-// const TOKEN = process.env.TOKEN;
 
 const API_KEY = process.env.GOOGLE_API_KEY;
 const yelpApiKey = process.env.YELP_API_KEY;
@@ -28,13 +27,6 @@ client.on('error', err => console.error(err));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.get('/api/v1/admin', (req, res)
-
-//https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&key=AIzaSyDLlLO9AmKusj7eGw_rT8e2t7MYLROTG_I
-
-
-// app.get('/api/v1/admin', (req, res) => res.send(TOKEN === parseInt(req.query.token)));
 
 app.get('/api/v1/get_stores', (req, res) => {
   const searchRequestWeed = {
@@ -79,6 +71,5 @@ app.get('/api/v1/get_fast_food', (req, res) => {
 });
 
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
-
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
